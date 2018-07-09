4ab5939cb402a40c4acac011c0a3030bc7ef1e37
Xiang Lou <xlou@hp.com>
2018-07-09 16:14:58 +0800

    project initialized

---
+++ manage.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 9, 2018
+
+@author: louloulx
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hello", methods=['GET'])
+def callback():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
